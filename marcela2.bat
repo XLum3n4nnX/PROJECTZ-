72ac01e00f8d05448b2ee69fbd24c6dc673eedd5
XLum3n4nnX <146385639+XLum3n4nnX@users.noreply.github.com>
2023-09-28 17:08:58 -0300

    Add files via upload

---
+++ marcela2.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Carregando...
+timeout /t 2 > nul
+cls
+echo Carregando...
+timeout /t 2 > nul
+cls
+echo Carregando...
+timeout /t 2 > nul
+cls
+echo Bem-vindo ao universo de Marcela!
+echo.
+echo Meu nome e Marcela e sou apaixonada por varias coisas, incluindo acampar, computadores, coisas hackers, boa comida e musica.
+echo.
+echo Escrever sempre foi uma das atividades que mais me dao prazer! Atraves dessa paixao, desenvolvi habilidades para escrita criativa, academica e todo o genero de criacao utilizando as palavras. Alem disso, sou uma entusiasta da tecnologia, e minha jornada me levou a explorar areas como hacking etico e programacao.
+echo.
+echo Quando nao estou diante do computador, voce pode me encontrar acampando na natureza, experimentando novas iguarias culinarias ou simplesmente curtindo minha playlist musical favorita.
+echo.
+echo Se voce compartilha alguma dessas paixoes, voce esta no lugar certo! Fique a vontade para explorar meu repositorio e descobrir como todas essas coisas se conectam em meu mundo.
+timeout /t 5 > nul
